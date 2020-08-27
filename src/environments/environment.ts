5eb1846d5e810ca6eab0b52dd7603e89ea74872e
Revanth <revanth54@gmail.com>
2020-08-26 19:14:19 -0600

    added firebase environmanet

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC1EzYTkny63bTu187M6LDL-rUm-fGvjbM",
+    authDomain: "bugtracker-6c224.firebaseapp.com",
+    databaseURL: "https://bugtracker-6c224.firebaseio.com",
+    projectId: "bugtracker-6c224",
+    storageBucket: "bugtracker-6c224.appspot.com",
+    messagingSenderId: "300692951442",
+    appId: "1:300692951442:web:a69e9f2be486573ca0314b",
+    measurementId: "G-R5LTQH69SW"
+  }
 };
 
 /*
