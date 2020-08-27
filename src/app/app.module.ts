bf4e374011d63006c261ece87793dd746618a8c3
Revanth <revanth54@gmail.com>
2020-08-26 19:14:55 -0600

    added imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,14 @@ import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatSelectModule} from '@angular/material/select';
+import {MatTableModule} from '@angular/material/table';
+import {MatListModule} from '@angular/material/list';
+
+import {environment} from '../environments/environment';
+import {AngularFireModule} from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+import {AngularFireAuthModule} from '@angular/fire/auth';
 
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -20,6 +28,10 @@ import { LoginRegisterComponent } from './components/layouts/login-register/logi
 import { ToolbarComponent } from './components/layouts/toolbar/toolbar.component';
 import { SearchComponent } from './components/search/search.component';
 import { BugcreateComponent } from './components/bugcreate/bugcreate.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { DatatableComponent } from './components/datatable/datatable.component';
+import { DatalistComponent } from './components/datalist/datalist.component';
+
 
 @NgModule({
   declarations: [
@@ -29,7 +41,11 @@ import { BugcreateComponent } from './components/bugcreate/bugcreate.component';
     LoginRegisterComponent,
     ToolbarComponent,
     SearchComponent,
-    BugcreateComponent
+    BugcreateComponent,
+    ProjectsComponent,
+    DatatableComponent,
+    DatalistComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -42,7 +58,14 @@ import { BugcreateComponent } from './components/bugcreate/bugcreate.component';
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatSelectModule
+    MatSelectModule,
+    MatTableModule,
+    MatListModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig,'BugTracker'),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
