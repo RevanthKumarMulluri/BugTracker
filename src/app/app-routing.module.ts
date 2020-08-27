f2f71582af65e3f1b6956ff89ed5a95038ffbfad
Revanth <revanth54@gmail.com>
2020-08-26 19:15:09 -0600

    added routing for create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginRegisterComponent } from './components/layouts/login-register/login-register.component';
-import { BugcreateComponent } from './components/bugcreate/bugcreate.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
 
 const routes: Routes = [
   {path: 'login',component:LoginRegisterComponent},
-  {path: 'create',component:BugcreateComponent}
+  {path: 'projects',component:ProjectsComponent}
 ];
 
 @NgModule({
